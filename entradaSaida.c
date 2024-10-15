c68417b6c4e89488057070a924725413b62cdc7b
NicolasTeles <nicolasrmendoncat@gmail.com>
2024-10-15 15:03:11 -0300

    first commit

---
+++ entradaSaida.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+
+void obterNomeArquivos(int argc, char* argv[], char** arquivoKit, char** arquivoConfiguracao){
+    int opt;
+    while((opt = getopt(argc, argv, "k:c:"))){
+        switch (opt){
+            case 'c':
+                *arquivoConfiguracao = optarg;
+                break;
+            case 'k':
+                *arquivoKit = optarg;
+                break
+            default:
+                break;
+        }
+    }
+    return 0;
+}
